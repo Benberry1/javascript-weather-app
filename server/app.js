cd2f1a59aba863245bcd3b2c77a1dc4e22bd8a3c
Ben Berry <ben.berry@developersinstitute.co.nz>
2022-07-13 16:19:19 +1200

    node.js api call to weatherapi.com return current weather data

--- server/app.js
+++ server/app.js
@@ -3,13 +3,33 @@ if (process.env.NODE_ENV !== 'production') {
 }
 const express = require('express');
 const app = express();
-const axios = require('axios');
+const fetch = require('node-fetch');
 
 const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
 
 app.use(express.json());
 app.use(express.static('../public'));
 
-app.post('/weather/api', (req, res, error) => {});
+app.post('/api/weather/current', async (req, res) => {
+  try {
+    const url = `http://api.weatherapi.com/v1/current.json?key=${WEATHER_API_KEY}&q=${req.body.latitude},${req.body.longitude}`;
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    });
+    if (response.ok) {
+      const data = await response.json();
+      return res.status(200).send(data);
+    } else {
+      throw new Error('Something went wrong!');
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send('Something went wrong! Please try again');
+  }
+});
 
 module.exports = app;
